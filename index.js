1178258bfef7582048951e3bdc0788a4a8dc9941
ValentinGTrapaga <gonzaleztrapagav@gmail.com>
2022-10-05 02:51:33 -0300

    add frontend - deploy

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 
 app.use(express.json())
+app.use(cors())
+app.use(express.static('dist'))
+
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({ error: 'Unknown endpoint' })
+}
 
 let notes = [
   {
@@ -70,7 +77,9 @@ app.post('/api/notes', (request, response) => {
   response.json(note)
 })
 
-const PORT = process.env.port || 3001
+app.use(unknownEndpoint)
+
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on PORT ${PORT}`)
 })
